e153eb4aa48afe3fd7fa6fcc7d973b6a05563051
Jack Newman <jack.newman96@gmail.com>
2018-02-20 10:53:43 -0800

    Add scripts to generate random and consecutive numbers.

---
+++ generateC.sh
@@ -0,0 +1,15 @@
+# This program will generate numbers consecutively from 1 to n in file file.txt
+
+# Usage: bash generateC.sh <file.txt> <n>
+
+
+touch $1
+echo "file: $1 created"
+
+echo "Generating..."
+for i in $(seq $2)
+do
+	echo $i >> $1
+done
+
+echo "Done."
