0a832058dd910357abf7a0d562292b92b6e64fd2
john-salman <31483839+john-salman@users.noreply.github.com>
2018-02-22 13:48:10 -0800

    created consecutive.py
    just converted the c++ file and changed it to track the average-case efficiency as well

---
+++ consecutive.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python                                                                                                                                                         
+
+import sys
+
+def main():
+  file = open('m_numbers.txt', 'r')
+  m_numbers = []
+  n_numbers = []
+  for line in file:
+      if line >= 3:
+          m_numbers.append(int(line))
+      n_numbers.append(int(line))
+  file.close()
+
+  i = 0
+  divisions = []
+  while i < len(m_numbers):
+    m = m_numbers[i]
+    i += 1
+
+    j = 0
+    n = n_numbers[j]
+    while m >= n:
+        divisions.append(consecutive(m,n))
+        j += 1
+        n = n_numbers[j]
+    average = sum(divisions) / float(m)
+    string_out = "Average-case efficiency of Consecutive-Integer Algortihm on input size: " + repr(m) + " is: " + repr(average)
+    print (string_out)
+
+# Input: integer _m, integer _n                                                                                                                                           
+# Condition: _m >= _n & n != 0                                                                                                                                            
+def consecutive(_m,_n):
+    m = _m
+    n = _n
+    r = _n
+    divisions = 0
+    while r > 1 :
+            if (m % r == 0 and n % r == 0) :
+              divisions += 2
+              return divisions # this is the gcd                                                                                                                          
+            r -= 1 # else go down by one                                                                                                                                  
+    return divisions
+
+
+
+if __name__== "__main__":
+      main()
+
