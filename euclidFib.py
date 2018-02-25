4b1af03b8eb563d78760412dcbae30d44f61b952
john-salman <31483839+john-salman@users.noreply.github.com>
2018-02-25 12:26:25 -0800

    Create euclidFib.py
    The implementation of Euclid's for task 2 need to be a little different because we want to count the divisions for each subsequent pair.
    Used to be gcd(m, n) with n = 1 -> m
    Now is gcd(m, n) with m = F(k), and n = F(k - 1)

---
+++ euclidFib.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python                                                                                                                                                         
+
+import sys
+
+def main():
+  file_name = sys.argv[1]
+  file = open(file_name, 'r')
+  numbers = []
+  for line in file:
+    numbers.append(int(line))
+  file.close()
+
+  i = 2 # because we want an n >= 1                                                                                                                                       
+  divisions = [] # to hold the number of divisions for each m, n pair                                                                                                     
+  while i < len(numbers):
+    m = numbers[i] # implementing it like this instead of F(k + 1), as its more readable                                                                                  
+    n = numbers[i - 1]
+    divisions.append(euclid(m,n))
+    i += 1
+  # we may want to rewrite this to calculate the gcd of several sequences of differing length                                                                             
+  # or perhaps we want to put more emphasis on the number of divisions for each pair                                                                                      
+  average = sum(divisions) / float(m)
+  string_out = "Worst-case efficiency of Euclids Algortihm on a Fibonacci Sequence of length: " + repr(len(numbers)) + " is: " + repr(average)
+  print (string_out)
+
+# Input: integer _m, integer _n                                                                                                                                           
+# Condition: _m >= _n                                                                                                                                                     
+def euclid(_m,_n):
+  m = _m
+  n = _n
+
+  divisions = 0
+  # If the value of n, then we know that the current m                                                                                                                    
+  # is the greatest common divider of both m & n                                                                                                                          
+  while n != 0 :
+    r = m % n
+    divisions += 1
+    m = n
+    n = r
+
+  return divisions # return the current m                                                                                                                                 
+
+
+if __name__== "__main__":
+      main()
+
