c1be930222ae10e5f7582588406ea5f0c9dbcb7d
Jack Newman <newmjack@sonoma.edu>
2018-03-01 14:55:36 -0800

    Add scatter-plot for euclids

--- task1/euclid.py
+++ task1/euclid.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python                                                                                                                                                         
 
 import sys
+import os
+import matplotlib.pyplot as plt
+
 
 def main():
   file_name = sys.argv[1] # our input comes in from a file provided at the command line                                                                                   
@@ -14,22 +17,42 @@ def main():
   divisions = [] # this list holds the number of divisions required to compute the gcd(m,n)                                                                               
   averages = [] # array to hold the average case efficiency for each value of m                                                                                           
   while i < len(numbers):
+    divisionCount = 0
     m = numbers[i] # hold the m constant for each loop                                                                                                                    
     i += 1
 
     j = 0
     n = numbers[j] # start the n values at 1 for each value of m                                                                                                          
-    while m >= n: # essentially gcd(m,m)                                                                                                                                  
-        divisions.append(euclid(m,n)) # count the divisions for this pair                                                                                                 
+    while m > n: # essentially gcd(m,m)                                                                                                                                  
+        divisionCount += euclid(m,n) # count the divisions for this pair                                                                                                 
         j += 1
         n = numbers[j] # increase n's value by one                                                                                                                        
 
-    average = sum(divisions) / float(m) # following the average-case efficiency formula from the spec                                                                     
+    divisions.append(divisionCount)
+    average = divisionCount / float(m) # following the average-case efficiency formula from the spec                                                                     
     averages.append(average) # we need to store these values for use in the scatter plot                                                                                  
-    string_out = "Average-case efficiency of Euclids Algorithm on input size: " + repr(m) + " is: " + repr(average)
-    print (string_out)
+    # string_out = "Average-case efficiency of Euclids Algorithm on input size: " + repr(m) + " is: " + repr(average)
+    # print (string_out)
 
-    # here is where we would place the matplotlib code                                                                                                                    
+
+    # print(numbers)
+    # print(divisions)
+    # print(averages)
+
+    # here is where we would place the matplotlib code 
+  _numbers = numbers[2:]
+  # print(_numbers)
+
+  plt.scatter(_numbers, averages)
+  plt.xlabel("M")
+  plt.ylabel("averages")
+  plt.tight_layout()
+  if(sys.argv[2] == "save"):
+    imgname = os.path.splitext(sys.argv[1])[0] + ".png"
+    plt.savefig(imgname)
+  else:
+    plt.show()
+
 
 
 # Input: integer _m, integer _n                                                                                                                                           
