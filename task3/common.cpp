27fe929da36ab7a000d582a37751f7d326a116c4
Jack Newman <newmjack@sonoma.edu>
2018-03-01 12:41:38 -0800

    Add working functionality to common.cpp

--- task3/common.cpp
+++ task3/common.cpp
@@ -1,11 +1,5 @@
 /*
 Usage: ./script.x <list file 1> <list file 2> <return file list OPTIONAL>
-*/
-
-
-
-
-/*
 
 ** MUST ** be used on sorted data sets
 
@@ -17,6 +11,48 @@ Current iteration will accept numbers on their own lines.
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <list>
+#include <string>
+
+std::list<int> printList(std::list<int> _list) {
+	for (std::list<int>::iterator it=_list.begin(); it != _list.end(); ++it) {
+		std::cout << *it << " " << std::endl;
+	}
+}
+
+std::list<int> findCommonNums(std::list<int> _list1, std::list<int> _list2) {
+	std::cout << "For input size N = " << (_list1.size() + _list2.size());
+	int loopCounter = 0;
+
+	// Create list for nums
+	std::list<int> commonNums;
+
+	// Create iterator itr1 and itr2 for list1 and list2 respectively.
+	std::list<int>::iterator itr1 = _list1.begin(), itr2 = _list2.begin();
+
+	while(!_list1.empty() && !_list2.empty()) {
+		loopCounter++;
+		if(*itr1 < *itr2) { //case in which a is less than b; pop A
+			++itr1;
+			_list1.pop_front();
+		}
+		else if(*itr2 < *itr1) { //case in which b is less than a; pop b
+			++itr2;
+			_list2.pop_front();
+		}
+		else {
+			commonNums.push_back(*itr1);
+			++itr1;
+			++itr2;
+			_list1.pop_front();
+			_list2.pop_front();
+		}
+	}
+	std::cout << " our loop ran " << loopCounter << " times." << std::endl;
+	return commonNums;
+}
+
+
 
 int main(int argc, char const *argv[]) {
 
@@ -39,29 +75,35 @@ int main(int argc, char const *argv[]) {
 		return 1; // The files do not exist.
 	}
 
-	// Create queue to house the data sets.
+	// Create list to house the data sets.
+	// Decided to go with list because you can push/pop from both front and back
+	// as well as being able to sort with a list function; Sorted input is required
+	// for our algorithm.
+	std::list<int> list1, list2;
+	std::string numIn;
 
-	// Assure the data sets are sorted, if not: sort.
+	// Insert Data from file into respective list
+	while(getline(file1, numIn)) {
+		list1.push_back(stoi(numIn));
+	}
+	file1.close();
+	while(getline(file2, numIn)) {
+		list2.push_back(stoi(numIn));
+	}
+	file2.close();
+
+	// Assure the data sets are sorted.
+	list1.sort();
+	list2.sort();
+
+	// Copy lists as commonalities function will destroy the lists TODO: decide if this is needed? We may not need the lists after function call
+	std::list<int> list1_c = list1, list2_c = list2;
 
 	// Commonalities function call
+	std::list<int> commonNums = findCommonNums(list1_c, list2_c);
 
-	// Cout commonalities function return if no output file
-	// is given otherwise:
-
-	// print commonalities array to output file
+	//Print the common numbers
+	// printList(commonNums);
 
 	return 0;
 }
-
-
-/* Commonalities function skeleton   (queue n, queue m)
-
-	Create new array to house commonalities which
-	at most can be of size(n + m).
-
-	iterate through both queues popping the lesser value.
-	and adding equal values to the commonalities array.
-
-
-*/
-
