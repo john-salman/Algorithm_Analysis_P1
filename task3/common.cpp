0a63a2e7b027708f19a531585e99d597a38446b5
Jack Newman <newmjack@sonoma.edu>
2018-02-28 22:41:34 -0800

    Flesh out common skeleton

--- task3/common.cpp
+++ task3/common.cpp
@@ -39,9 +39,29 @@ int main(int argc, char const *argv[]) {
 		return 1; // The files do not exist.
 	}
 
-	// Create arrays to house the data sets.
+	// Create queue to house the data sets.
 
+	// Assure the data sets are sorted, if not: sort.
 
+	// Commonalities function call
+
+	// Cout commonalities function return if no output file
+	// is given otherwise:
+
+	// print commonalities array to output file
 
 	return 0;
 }
+
+
+/* Commonalities function skeleton   (queue n, queue m)
+
+	Create new array to house commonalities which
+	at most can be of size(n + m).
+
+	iterate through both queues popping the lesser value.
+	and adding equal values to the commonalities array.
+
+
+*/
+
