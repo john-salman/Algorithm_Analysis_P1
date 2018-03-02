5b64b34a43c8ef447e525c78bb9789c9db89a8ad
Jack Newman <newmjack@sonoma.edu>
2018-03-02 10:44:56 -0800

    add pictures of scatter plots and various data

--- task1/consecutive.py
+++ task1/consecutive.py
@@ -9,51 +9,60 @@ import matplotlib.pyplot as plt
 def main():
   file_name = sys.argv[1]
   file = open(file_name, 'r')
-  m_numbers = []
-  n_numbers = []
-  averages = []
+  numbers = []
   for line in file:
-      if line >= 3:
-          m_numbers.append(int(line))
-      n_numbers.append(int(line))
+      numbers.append(int(line))
   file.close()
 
-  i = 0
+  i = 2
   divisions = []
-  while i < len(m_numbers):
-    m = m_numbers[i]
+  averages = []
+  while i < len(numbers):
+    print(numbers[i])
+    divisionCount = 0
+    m = numbers[i]
     i += 1
 
     j = 0
-    n = n_numbers[j]
+    n = numbers[j]
     while m > n:
-        divisions.append(consecutive(m,n))
+        divisionCount += consecutive(m,n)
         j += 1
-        n = n_numbers[j]
-    average = sum(divisions) / float(m)
-    string_out = "Average-case efficiency of Consecutive-Integer Algortihm on input size: " + repr(m) + " is: " + repr(average)
+        n = numbers[j]
+
+    divisions.append(divisionCount)
+
+    average = divisionCount / float(m)
     averages.append(average)
+    # string_out = "Average-case efficiency of Consecutive-Integer Algortihm on input size: " + repr(m) + " is: " + repr(average)
     # print (string_out)
 
-  print(divisions)
-  print(numbers)
-  print(average)
+  # print(divisions)
+  # print(averages)
+  _numbers = numbers[2:]
+  plt.scatter(_numbers, averages)
+  plt.xlabel("M")
+  plt.ylabel("averages")
+  plt.tight_layout()
+  imgname = os.path.splitext(sys.argv[1])[0] + "png"
+  plt.savefig(imgname)
+  # plt.show()
 
 # Input: integer _m, integer _n                                                                                                                                           
 # Condition: _m >= _n & n != 0                                                                                                                                            
+
+# # # I HAVE PULLED THIS DIRECTLY FROM SECTION 1.1 EXACTLY.
 def consecutive(_m,_n):
-    m = _m
-    n = _n
-    r = _n
-    divisions = 0
-    while r > 1 :
-            if (m % r == 0 and n % r == 0) :
-              divisions += 2
-              return divisions # this is the gcd                                                                                                                          
-            r -= 1 # else go down by one                                                                                                                                  
-    return divisions
-
-
+  divisions = 0
+  t = min(_m,_n)
+  while t > 0:
+    if _m % t == 0:
+      divisions += 1
+      if _n % t == 0:
+        divisions += 1
+        break;
+    t -= 1
+  return divisions
 
 if __name__== "__main__":
       main()
