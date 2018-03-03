8f1efe5c60a92218d28534b05c7d66dab1ae049f
Jack Newman <newmjack@sonoma.edu>
2018-03-02 18:17:48 -0800

    Add complete functionality for task3 scatterplot + clean up directory of task3

--- task3/makeScatterPlot.py
+++ task3/makeScatterPlot.py
@@ -7,17 +7,21 @@ import os
 def main():
   file_name = sys.argv[1]
   file = open(file_name, 'r')
-  numbers = []
+  time_averages = []
   for line in file:
-    numbers.append(int(line))
+    time_averages.append(int(line))
   file.close()
 
+  N_numbers = []
+  i = 1
+  while (i < len(time_averages + 1)):
+    N_numbers.append(int(i))
+    i += 1
 
 
-
-  plt.scatter(fib_numbers, divisions)
-  plt.xlabel("Fibonacci Numbers")
-  plt.ylabel("Number of modulo divisions")
+  plt.scatter(N_numbers, time_averages)
+  plt.xlabel("N")
+  plt.ylabel("Time")
   plt.tight_layout()
   if(sys.argv[2] == "save"):
     imgname = os.path.splitext(sys.argv[1])[0] + ".png"
